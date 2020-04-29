8b525b86b9ac0d4e65de126992de37304f8dd476
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-04-29 19:47:34 -0300

    (#5) Criando insert de usuario no banco

--- pos-java-jdbc/src/main/java/dao/UserPosDAO.java
+++ pos-java-jdbc/src/main/java/dao/UserPosDAO.java
@@ -1,15 +1,39 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import conexaojdbc.SingleConnection;
+import model.Userposjava;
 
 public class UserPosDAO {
-	
+
 	private Connection connection;
-	
+
 	public UserPosDAO() {
 		connection = SingleConnection.getConnection();
+	}
+
+	public void salvar(Userposjava userposjava) {
+		try {
+			String sql = "insert into userposjava (id, nome, email) values (?,?,?)";
+			PreparedStatement insert = connection.prepareStatement(sql);
+
+			insert.setLong(1, userposjava.getId());
+			insert.setString(2, userposjava.getNome());
+			insert.setString(3, userposjava.getEmail());
+			insert.execute();
+			connection.commit();
+
+		} catch (SQLException e) {
+			try {
+				connection.rollback();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 	}
 
 }
--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -2,13 +2,21 @@ package pos_java_jdbc.pos_java_jdbc;
 
 import org.junit.Test;
 
-import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TesteBancoJdbc {
 	
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(3L);
+		userposjava.setNome("Andre DAO");
+		userposjava.setEmail("andredao@gmail.com");
+		
+		userPosDAO.salvar(userposjava);
 	}
 
 }
